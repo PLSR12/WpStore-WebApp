cd4bf87ab8dedd205d51a41657f4d503ad1f02c6
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-03-20 20:30:14 -0300

    feature: finished list of allProducts

--- src/components/pages/Products/index.tsx
+++ src/components/pages/Products/index.tsx
@@ -1,34 +1,54 @@
-import { Card, Col, Row } from "antd"
+import { Card, Col, Rate, Row } from "antd"
 import Meta from "antd/es/card/Meta"
+import { maskCurrency } from "common/utils/maskCurrency"
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import { ProductsContext } from "store/context/ProductsContext"
 
 export const Products: React.FC = () => {
-  const { products } = useContext(ProductsContext)
+  const { products, isLoading } = useContext(ProductsContext)
   const navigate = useNavigate()
   console.log(products)
+
   return (
-    <Row gutter={16} justify={"center"}>
+    <Row gutter={24} justify={"center"}>
       {products.map((product: any) => {
         return (
           <Col xs={24} sm={12} md={6}>
             <Card
-              style={{ height: "min-content", marginTop: 25, marginBottom: 25 }}
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 35,
+              }}
+              loading={isLoading}
               hoverable
+              bordered
+              extra={
+                <>
+                  <Rate disabled value={Math.round(product.rating.rate)} />
+                </>
+              }
               cover={
                 <img
                   alt={product.title}
                   src={product.image}
                   style={{
-                    width: "100%",
-                    height: "45vh",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: 10,
+                    paddingLeft: 40,
+                    maxWidth: "80%",
+                    height: "230px",
                   }}
                 />
               }
               onClick={() => navigate(`/product/:${product.id}`)}
             >
-              <Meta title={product.title} description={product.price} />
+              <Meta
+                title={product.title}
+                description={<strong>{maskCurrency(product.price)}</strong>}
+              />
             </Card>
           </Col>
         )
